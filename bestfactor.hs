395ce97c346d11f3096754b1eab32ac5e9fa6ecc
Paul Alfille <paul.alfille@gmail.com>
2023-10-25 23:46:15 -0400

    Add rstage type

--- bestfactor.hs
+++ bestfactor.hs
@@ -11,10 +11,38 @@ isprime p
           | mod q r == 0 = False
           | otherwise = ip q (r+2)
 
+order :: Int -> Int -> Int
 order n p
   | mod n p == 0 = 0
   | otherwise = 1 + order (div n p) p
 
 primes = take prime_length $ filter isprime [2..]
 
-order_sequence r m p
+rising [] = []
+rising [x] = [x]
+rising (x:xs)
+  | x<(head xs) = (x:rising xs)
+  | otherwise = [x]
+
+data Recip = Recip {
+  modulus:: Int ,
+  remainder:: Int,
+  prime:: Int
+  } deriving (Show)
+
+data RStage = RStage {
+  rEcip:: Recip,
+  iNdex:: Int,
+  vAlue:: Int
+  } deriving(Show)
+
+rOrder (RStage { rEcip= Recip {prime=p}, vAlue=n }) = order n p
+
+nextStage (RStage (Recip m r p) i v) =
+  RStage (Recip m r p) (i+1) (v + m * p^(i+1) ) 
+
+firstStage rc@(Recip m r p) = (RStage rc 1 (tst r))
+  where tst v
+          | mod v p == 0 = v
+          | otherwise = tst (v+m)
+ 
